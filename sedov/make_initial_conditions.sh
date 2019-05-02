4c988807bc184fedb40c1e5a5aef379c457bc7d2
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-05-02 15:19:55 +0100

    Added files required for sedov runs

---
+++ sedov/make_initial_conditions.sh
@@ -0,0 +1,28 @@
+for npart in 8 16 32 64 128 256
+do
+    mkdir $npart
+    cd $npart
+
+    total_npart=$(($npart*$npart*$npart))
+
+    for scheme in gadget2 minimal pressure-energy anarchy-pu gizmo-mfm gizmo-mfv
+    do
+        mkdir $scheme
+        cd $scheme
+
+        echo "Creating initial conditions for ${npart}^3 (${total_npart}) particles with ${scheme}"
+
+        for kernel in cubic-spline quintic-spline wendland-C2
+        do
+            mkdir $kernel
+            cd $kernel
+
+            python3 ../../../makeIC.py ../../../../../build/glass_files/$scheme/$kernel/glassCube_$npart.hdf5
+
+            cd ..
+        done
+
+        cd ..
+    done
+    cd ..
+done
