f6db62d50040824e00b66f5c905b11881a7ad99e
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-05-02 15:29:09 +0100

    Added SodShock initial conditions creation script

---
+++ sod/make_initial_conditions.sh
@@ -0,0 +1,31 @@
+for npart in 16 32 64 128
+do
+    mkdir $npart
+    cd $npart
+
+    next_npart=$((npart*2))
+
+    total_npart=$(($npart*$npart*$npart))
+
+    for scheme in gadget2 minimal pressure-energy anarchy-pu gizmo-mfm gizmo-mfv
+    do
+        mkdir $scheme
+        cd $scheme
+
+        echo "Creating initial conditions for ${npart}^3 (${total_npart}) particles with ${scheme}"
+
+        for kernel in cubic-spline quintic-spline wendland-C2
+        do
+            mkdir $kernel
+            cd $kernel
+
+            python3 ../../../makeIC.py ../../../../../build/glass_files/$scheme/$kernel/glassCube_$next_npart.hdf5 \
+                                       ../../../../../build/glass_files/$scheme/$kernel/glassCube_$npart.hdf5
+
+            cd ..
+        done
+
+        cd ..
+    done
+    cd ..
+done
